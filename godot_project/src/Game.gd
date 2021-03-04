f9f607081047dbd815d6fdb96ea244fa9d5f2bf0
Krunoslav Gregorec <krunoslav.gregorec@gmail.com>
2021-03-04 16:05:12 +0100

    Implement logic for setting highscore list UI data Implement update for highscore list data structure in State

--- godot_project/src/Game.gd
+++ godot_project/src/Game.gd
@@ -16,17 +16,21 @@ const SCENE_TOWN := preload("res://src/game/Town.tscn")
 onready var _game_timer := $Timer
 # UI STUFF
 onready var _ui_root := $UI/VBoxContainer
-
-onready var _highscore_input_UI := $UI/VBoxContainer/HighscoreInput
-onready var _name_input_label := $UI/VBoxContainer/HighscoreInput/NameInputLabel
-onready var _button_submit := $UI/VBoxContainer/HighscoreInput/ButtonSubmit
+onready var _viewport := $ViewportContainer
 
 onready var _score_value := $UI/VBoxContainer/Highscore/ScoreValue
 onready var _buttons := $UI/VBoxContainer/Buttons
 onready var _button_restart := $UI/VBoxContainer/Buttons/ButtonRestart
 onready var _button_main_menu := $UI/VBoxContainer/Buttons/ButtonMainMenu
 
-onready var _viewport := $ViewportContainer
+onready var _highscore_list := $UI/VBoxContainer/HighscoreList
+onready var _highscore_labels := _highscore_list.get_children()
+
+onready var _highscore_input_UI := $UI/VBoxContainer/HighscoreInput
+onready var _name_input_label := $UI/VBoxContainer/HighscoreInput/NameInputLabel
+onready var _button_submit := $UI/VBoxContainer/HighscoreInput/ButtonSubmit
+
+
 
 var _town : classTown
 var _last_highscore = 0
@@ -64,18 +68,34 @@ func _finish_game() -> void:
 		_last_highscore = score
 		# Enable UI for name input
 		_highscore_input_UI.show()
+		_highscore_list.hide()
+		_buttons.hide()
+
 		KeyboardBackend.clear_input_buffer_on_hide = false
 		KeyboardBackend.set_visible(true)
-		_buttons.hide()
 	else:
 		KeyboardBackend.clear_input_buffer_on_hide = true
 		KeyboardBackend.set_visible(false)
+		_update_highscore_list()
 		_highscore_input_UI.hide()
+		_highscore_list.show()
 		_buttons.show()
 
 	# Display end game UI
 	_ui_root.show()
 	_viewport.hide()
+
+func _update_highscore_list() -> void:
+	for highscore_label in _highscore_labels:
+		highscore_label.get_node("PlayerName").text = ""
+		highscore_label.get_node("PlayerScore").text = ""
+		highscore_label.hide()
+
+	var highscores : Array = State.get_highscores()
+	for i in highscores.size():
+		_highscore_labels[i].get_node("PlayerName").text = str(highscores[i].get("name"))
+		_highscore_labels[i].get_node("PlayerScore").text = str(highscores[i].get("score"))
+		_highscore_labels[i].show()
 
 
 ################################################################################
@@ -92,6 +112,8 @@ func _on_main_menu_button_pressed() -> void:
 func _on_highscore_input_submit_button_pressed() -> void:
 	State.set_highscore(_last_highscore, KeyboardBackend.input_buffer)
 	KeyboardBackend.input_buffer = ""
+	_update_highscore_list()
+	_highscore_list.show()
 	_highscore_input_UI.hide()
 	_buttons.show()
 
--- godot_project/src/autoload/State.gd
+++ godot_project/src/autoload/State.gd
@@ -5,20 +5,34 @@ extends Node
 ## SIGNALS
 signal state_changed
 
+
 ################################################################################
 ## PUBLIC VARIABLES
-var current_highscore := {}
+var current_highscores := []
 
+
+################################################################################
+## PUBLIC FUNCTIONS
 func set_highscore(score : int, player_name : String) -> void:
 	if player_name == "":
 		return
-	current_highscore[player_name] = score
+	current_highscores.append({"name": player_name, "score": score})
+	current_highscores.sort_custom(self, "_sort_highscore")
 	emit_signal("state_changed")
 
+func get_highscores() -> Array:
+	return current_highscores
+
 func is_highscore_set(score : int) -> bool:
-	if not current_highscore or current_highscore.empty():
+	if not current_highscores or current_highscores.empty() or current_highscores.size() < 10:
 		return true
-	for highscore in current_highscore:
-		if current_highscore[highscore] < score:
+	for i in current_highscores:
+		if current_highscores[i].get("score") < score:
 			return true
+	return false
+
+
+func _sort_highscore(value_a : Dictionary, value_b : Dictionary) -> bool:
+	if value_a.get("score") > value_b.get("score"):
+		return true
 	return false
