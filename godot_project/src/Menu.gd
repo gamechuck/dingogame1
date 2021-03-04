ce5ee18226d7ded536ec3cc2c12be7518be3bf2e
Krunoslav Gregorec <krunoslav.gregorec@gmail.com>
2021-03-04 19:24:56 +0100

    Trafo collision shape position fix Assets of thief and dingo reimport with new settings to tackle issue of dithering Add quit button on starting screen

--- godot_project/src/Menu.gd
+++ godot_project/src/Menu.gd
@@ -1,16 +1,19 @@
 extends Control
 
-onready var _start_button := $StartButton
 onready var _highscore_value_label := $HighscorePanel/HBoxContainer/Value
 
 
 func _ready():
-	_start_button.connect("pressed", self, "_on_start_button_pressed")
+	$StartButton.connect("pressed", self, "_on_start_button_pressed")
+	$QuitButton.connect("pressed", self, "_on_quit_button_pressed")
 	#_highscore_value_label.text = str(State.get_highscore())
 	State.connect("state_changed", self, "_on_state_changed")
 
 func _on_start_button_pressed():
 	Flow.change_scene_to("game")
+
+func _on_quit_button_pressed():
+	Flow.deferred_quit()
 
 func _on_state_changed() -> void:
 	_highscore_value_label.text = str(State.get_highscore())
